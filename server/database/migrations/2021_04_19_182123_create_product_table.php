2988b94271bd45ea28ee0ed765e75079c355c754
Denis Belik <denis.belik2014@gmail.com>
2021-04-19 21:32:31 +0300

    add product table migrations

---
+++ server/database/migrations/2021_04_19_182123_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+
+            $table->integer("weight");
+            $table->string("color");
+            $table->float('price');
+            $table->index('type_id');
+
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('product_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
